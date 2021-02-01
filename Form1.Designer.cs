b1687a1f22468a011f464526252a96177e23b8be
Timoy_7207 <jostimian@gmail.com>
2021-02-01 15:39:58 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace WeatherSearch
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.locationBar = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.getWeather = new System.Windows.Forms.Button();
+			this.longhText = new System.Windows.Forms.Label();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.latText = new System.Windows.Forms.Label();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// locationBar
+			// 
+			this.locationBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.locationBar.Location = new System.Drawing.Point(66, 29);
+			this.locationBar.Name = "locationBar";
+			this.locationBar.Size = new System.Drawing.Size(405, 22);
+			this.locationBar.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Location";
+			// 
+			// getWeather
+			// 
+			this.getWeather.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.getWeather.Location = new System.Drawing.Point(477, 29);
+			this.getWeather.Name = "getWeather";
+			this.getWeather.Size = new System.Drawing.Size(75, 23);
+			this.getWeather.TabIndex = 4;
+			this.getWeather.Text = "Search";
+			this.getWeather.UseVisualStyleBackColor = true;
+			this.getWeather.Click += new System.EventHandler(this.getWeather_Click);
+			// 
+			// longhText
+			// 
+			this.longhText.AutoSize = true;
+			this.longhText.Location = new System.Drawing.Point(13, 71);
+			this.longhText.Name = "longhText";
+			this.longhText.Size = new System.Drawing.Size(74, 13);
+			this.longhText.TabIndex = 6;
+			this.longhText.Text = "longhitude : ";
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// contextMenuStrip2
+			// 
+			this.contextMenuStrip2.Name = "contextMenuStrip2";
+			this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+			// 
+			// fIleToolStripMenuItem
+			// 
+			this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+			this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fIleToolStripMenuItem.Text = "FIle";
+			// 
+			// settingsToolStripMenuItem
+			// 
+			this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+			this.settingsToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+			this.settingsToolStripMenuItem.Text = "Preferences";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+			this.menuStrip1.Size = new System.Drawing.Size(557, 24);
+			this.menuStrip1.TabIndex = 5;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// latText
+			// 
+			this.latText.AutoSize = true;
+			this.latText.Location = new System.Drawing.Point(13, 97);
+			this.latText.Name = "latText";
+			this.latText.Size = new System.Drawing.Size(55, 13);
+			this.latText.TabIndex = 9;
+			this.latText.Text = "Latitude :";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(557, 311);
+			this.Controls.Add(this.latText);
+			this.Controls.Add(this.longhText);
+			this.Controls.Add(this.getWeather);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.locationBar);
+			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox locationBar;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button getWeather;
+		private System.Windows.Forms.Label longhText;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+		private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.Label latText;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherSearch
+{
+	public partial class Form1 : Form
+	{
+		String url = @"http://api.openweathermap.org/data/2.5/weather?q=";
+		String apiKey = "&appid=6a6bfcb730cdf7f8507bb5c849d13fe1";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public string getWeatherData(String location)
+		{
+			WebRequest req = WebRequest.Create(url + location + apiKey);
+			WebResponse resp = req.GetResponse();
+			using (Stream data = resp.GetResponseStream())
+			{
+				StreamReader reader = new StreamReader(data);
+				String dataout = reader.ReadToEnd();
+				return dataout;
+			}
+		}
+
+		private void getWeather_Click(object sender, EventArgs e)
+		{
+			String location = locationBar.Text;
+			String dataout = getWeatherData(location);
+			dynamic obj = JsonConvert.DeserializeObject<dynamic>(dataout);
+			longhText.Text = longhText.Text + obj["coord"]["lon"];
+			latText.Text = latText.Text + obj["coord"]["lat"];
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			locationBar.Text = Properties.Settings.Default.DefaultLocation;
+		}
+	}
+}
